119201282fa38bcc2138def3bd8fc3eac81e5ba5
KomlevaAlena <norgiss404@gmail.com>
2026-06-11 08:58:09 +0500

    fix: base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/Dashboard-app/',
+  base: '/RS-outsource-dashboard/',
   
   build: {
     outDir: 'docs',
