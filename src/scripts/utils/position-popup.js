0d7a7c21328f169d73f7cc6184473f60e65c740a
KomlevaAlena <norgiss404@gmail.com>
2026-07-23 11:44:30 +0500

    feat: add position-popup.js

---
+++ src/scripts/utils/position-popup.js
@@ -0,0 +1,8 @@
+/**
+ * src/scripts/utils/position-popup.js
+ * Функция для умного позиционирования выпадающих окон (popups/tooltips)
+ * 
+ * @param {HTMLElement} popup - Элемент поп-апа, который нужно позиционировать
+ * @param {HTMLElement} target - Элемент (кнопка/ячейка), относительно которого открывается поп-ап
+ */
+
